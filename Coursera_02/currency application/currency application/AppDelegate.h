e5d8497bd01882668380859f0480ca97bfda8db6
Wang Ziyi <elsinore@users.noreply.github.com>
2018-04-14 21:22:09 +0900

    Initial upload
    Initial upload

---
+++ Coursera_02/currency application/currency application/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  currency application
+//
+//  Created by 王紫仪 on 06/07/2017.
+//  Copyright © 2017 WangZiyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Coursera_02/currency application/currency application/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  currency application
+//
+//  Created by 王紫仪 on 06/07/2017.
+//  Copyright © 2017 WangZiyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
